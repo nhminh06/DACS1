7877549d1984e62213f8a9144eee7b04d7111efd
nhminh06 <minhnhn.24itb@vku.udn.vn>
2025-05-10 15:27:49 +0700

    giaodienlan1

---
+++ src/main/java/com/cafe/Main.java
@@ -0,0 +1,27 @@
+package com.cafe;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/com/cafe/view/Management.fxml"));
+        Scene scene = new Scene(root);
+        scene.getStylesheets().add(getClass().getResource("/com/cafe/view/Style.css").toExternalForm());
+        primaryStage.setScene(scene); primaryStage.setTitle("Cafe Order"); primaryStage.setWidth(1200); primaryStage.setHeight(900); primaryStage.setResizable(false); primaryStage.show();
+
+
+        primaryStage.setTitle("Cafe Order");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+}
