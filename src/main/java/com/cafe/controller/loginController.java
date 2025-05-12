c25942012adca34be5fadfe2e6fe126bc5d5dff2
SangBuocRaDuongVapVaoHonDaNgaVaoTimEm <hiiithaiii06@gmail.com>
2025-05-12 14:28:10 +0700

    okem

--- src/main/java/com/cafe/controller/loginController.java
+++ src/main/java/com/cafe/controller/loginController.java
@@ -40,7 +40,7 @@ public class loginController {
 
     public Connection getConnection() {
         try {
-            return DriverManager.getConnection("jdbc:mysql://localhost:3306/taikhoan", "root", "");
+            return DriverManager.getConnection("jdbc:mysql://localhost:13306/co", "root", "");
         } catch (Exception e) {
             e.printStackTrace();
             return null;
