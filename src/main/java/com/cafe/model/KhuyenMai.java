7fd34a5891460bf6d6cf1788c1fba2e5e79ca8a7
SangBuocRaDuongVapVaoHonDaNgaVaoTimEm <hiiithaiii06@gmail.com>
2025-05-13 20:52:29 +0700

    khuyenmai

---
+++ src/main/java/com/cafe/model/KhuyenMai.java
@@ -0,0 +1,41 @@
+package com.cafe.model;
+
+import javafx.beans.property.*;
+
+import java.time.LocalDate;
+
+public class KhuyenMai {
+    private final StringProperty tenMa;
+    private final IntegerProperty giaTri;
+    private final ObjectProperty<LocalDate> ngayHetHan;
+
+    public KhuyenMai(String tenMa, int giaTri, LocalDate ngayHetHan) {
+        this.tenMa = new SimpleStringProperty(tenMa);
+        this.giaTri = new SimpleIntegerProperty(giaTri);
+        this.ngayHetHan = new SimpleObjectProperty<>(ngayHetHan);
+    }
+
+    public StringProperty tenMaProperty() {
+        return tenMa;
+    }
+
+    public IntegerProperty giaTriProperty() {
+        return giaTri;
+    }
+
+    public ObjectProperty<LocalDate> ngayHetHanProperty() {
+        return ngayHetHan;
+    }
+    public String getTenMa() {
+        return tenMa.get();
+    }
+
+    public int getGiaTri() {
+        return giaTri.get();
+    }
+
+    public LocalDate getNgayHetHan() {
+        return ngayHetHan.get();
+    }
+}
+
